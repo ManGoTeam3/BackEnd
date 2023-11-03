06066130d92ea13327d9f60cd1508018470ddb64
leehaesong3966 <leehaesong3966@naver.com>
2023-11-04 01:44:02 +0900

    fix

--- src/main/java/com/mango/controller/RestaurantController.java
+++ src/main/java/com/mango/controller/RestaurantController.java
@@ -1,6 +1,5 @@
 package com.mango.controller;
 
-import com.mango.controller.inter.RestaurantController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 @RequiredArgsConstructor
-public class RestaurantController implements RestaurantController {
+public class RestaurantController  {
 }
