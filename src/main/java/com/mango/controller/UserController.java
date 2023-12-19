dcbe652d21b6a865091c24685d212b708ec0d289
kimYejin <kimphoby0125@gmail.com>
2023-12-19 22:30:28 +0900

    일반회원가입,로그인 구현

--- src/main/java/com/mango/controller/UserController.java
+++ src/main/java/com/mango/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.mango.controller;
 
+import com.mango.dto.UserInfoDto;
+import com.mango.dto.UserSignUpDto;
 import com.mango.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,24 +17,23 @@ import org.springframework.web.bind.annotation.*;
 public class UserController{
     private final UserService userService;
 
-//    @Value("${jwt.expiration}")
-//    private Long expireTime;
-//    @Operation(summary = "회원가입")
-//    @PostMapping("/api/user/join")
-//    public ResponseEntity join(@RequestBody UserSignUpDto userSignUpDto) {
-//        userService.signUp(userSignUpDto.getEmail(), userSignUpDto.getPassword(), userSignUpDto.getNickName());
-//        return ResponseEntity.ok().build();
-//    }
+    @Value("${jwt.expiration}")
+    private Long expireTime;
 
-//    @Operation(summary = "로그인")
-//    @PostMapping("/api/user/login")
-//    public ResponseEntity login(@RequestBody UserSignUpDto userSignUpDto) {
-//        String token = userService.login(userSignUpDto.getEmail());
-//        TokenDto tokenDto = new TokenDto();
-//        tokenDto.setAccessToken(token);
-//        tokenDto.setGrantType("Bearer");
-//        tokenDto.setAccessTokenExpiresIn(String.valueOf(expireTime));
-//        return ResponseEntity.ok(tokenDto);
-//    }
+    @Operation(summary = "회원가입")
+    @PostMapping("/api/user/join")
+    public ResponseEntity  signUp(@RequestBody UserSignUpDto userSignUpDto){
+        userService.signUp(userSignUpDto.getUsername(), userSignUpDto.getPassword());
+        return ResponseEntity.ok().build();
+    }
+    @Operation(summary = "로그인")
+    @PostMapping("/api/user/login")
+    public ResponseEntity login(@RequestBody UserSignUpDto userSignUpDto) {
+        String username = userSignUpDto.getUsername();
+        String password = userSignUpDto.getPassword();
+        String token = userService.login(username, password);
+        UserInfoDto user = new UserInfoDto(username,token);
+        return ResponseEntity.ok(user);
+    }
 }
 
